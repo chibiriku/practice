bd27187842ae6cea9855aa1a641088a9a0b5e7e0
chibiriku <96232950+chibiriku@users.noreply.github.com>
2023-03-29 23:37:49 +0900

    20230329_バインド途中

---
+++ src/main/java/com/example/form/SignupForm.java
@@ -0,0 +1,19 @@
+package com.example.form;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+	private String userId;
+	private String password;
+	private String userName;
+	
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Data birthday;
+	
+	private Integer age;
+	private Integer gender;
+
+}
