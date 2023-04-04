5632f912223a55d64f32f631785e6c633a01f3d7
chibiriku <96232950+chibiriku@users.noreply.github.com>
2023-04-04 22:30:53 +0900

    2023_validation順番

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -36,7 +37,7 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignup(Model model, @ModelAttribute @Validated SignupForm form, BindingResult bindingResult) {
+	public String postSignup(Model model, @ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult) {
 		
 		if(bindingResult.hasErrors()){
 		return  getSignup(model, form);
--- src/main/java/com/example/form/GroupOrder.java
+++ src/main/java/com/example/form/GroupOrder.java
@@ -1,5 +1,8 @@
 package com.example.form;
 
-public class GroupOrder {
+import jakarta.validation.GroupSequence;
+
+@GroupSequence({ValidGroup1.class, ValidGroup2.class})
+public interface GroupOrder {
 
 }
